09d1a389686b1fbeee919ab02d216862bd47b387
Noah Baumgartner <baumgartner.noah@outlook.com>
2026-09-23 20:31:07 +0200

    removed chromatic dep

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,11 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	addons: [
-		'@storybook/addon-svelte-csf',
-		'@chromatic-com/storybook',
-		'@storybook/addon-vitest',
-		'@storybook/addon-a11y',
-		'@storybook/addon-docs'
-	],
+        '@storybook/addon-svelte-csf',
+        '@storybook/addon-vitest',
+        '@storybook/addon-a11y',
+        '@storybook/addon-docs'
+    ],
 	framework: '@storybook/sveltekit'
 };
 export default config;
