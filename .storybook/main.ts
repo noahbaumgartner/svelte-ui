50bf4dbff78171aadc9c305cd1555689fb0cd1e0
Noah Baumgartner <baumgartner.noah@outlook.com>
2026-09-26 17:20:04 +0200

    removed interactions tab

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
         '@storybook/addon-a11y',
         '@storybook/addon-docs'
     ],
-	framework: '@storybook/sveltekit'
+	framework: '@storybook/sveltekit',
+	features: { interactions: false }
 };
 export default config;
